124b3f93f414bfbe9a9f421493e3f5643a87f3f8
EmreRauhe <55278194+EmreRauhe@users.noreply.github.com>
2020-01-20 13:56:51 +0100

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -31,17 +31,22 @@ namespace Yahtzee
             
         }
 
-        int Sum = 0;
-        
+        int totalnumber;
+        int number1;
+        int number2;
+        int number3;
+        int number4;
+        int number5;
+
         public void Throw_click(object sender, RoutedEventArgs e)
         {
 
             Random rnd = new Random();
-            int number1 = rnd.Next(1, 7);
-            int number2 = rnd.Next(1, 7);
-            int number3 = rnd.Next(1, 7);
-            int number4 = rnd.Next(1, 7);
-            int number5 = rnd.Next(1, 7);
+            number1 = rnd.Next(1, 7);
+            number2 = rnd.Next(1, 7);
+            number3 = rnd.Next(1, 7);
+            number4 = rnd.Next(1, 7);
+            number5 = rnd.Next(1, 7);
             dice1.Text = "";
             dice2.Text = "";
             dice3.Text = "";
@@ -62,9 +67,6 @@ namespace Yahtzee
             dice3.Text += diceroll3;
             dice4.Text += diceroll4;
             dice5.Text += diceroll5;
-
-
-            int totalnumber;
 
             string section1 = dice1.Text;
             string section2 = dice2.Text;
@@ -101,10 +103,10 @@ namespace Yahtzee
                  ((i[2] == i[3]) && (i[3] == i[4])) &&
                  (i[1] != i[2])))
             {
-                Sum = 25;
+                textboxFullHouseScore.Text = 25.ToString();
             }
 
-            textboxFullHouseScore.Text = Sum.ToString();
+            
 
             //three-of-a-kind
             if (number1 == 1 && number2 == 1 && number3 == 1 && number4 != 1 && number5 != 1 ||
@@ -149,14 +151,14 @@ namespace Yahtzee
 
                 
             }
-
-           else if ((number1 == 1 && number2 == 1 && number3 == 1 && number4 == 1) ||
+            //four-of-a-kind(Carré)
+            if ((number1 == 1 && number2 == 1 && number3 == 1 && number4 == 1) ||
                 (number1 == 2 && number2 == 2 && number3 == 2 && number4 == 2) ||
                 (number1 == 3 && number2 == 3 && number3 == 3 && number4 == 3) ||
                 (number1 == 4 && number2 == 4 && number3 == 4 && number4 == 4) ||
                 (number1 == 5 && number2 == 5 && number3 == 5 && number4 == 5) ||
                 (number1 == 6 && number2 == 6 && number3 == 6 && number4 == 6) ||
-                (number2 == 1 && number3 == 1 && number4 == 1 && number5 == 1) || 
+                (number2 == 1 && number3 == 1 && number4 == 1 && number5 == 1) ||
                 (number2 == 2 && number3 == 2 && number4 == 2 && number5 == 2) ||
                 (number2 == 3 && number3 == 3 && number4 == 3 && number5 == 3) ||
                 (number2 == 4 && number3 == 4 && number4 == 4 && number5 == 4) ||
@@ -164,39 +166,77 @@ namespace Yahtzee
                 (number2 == 6 && number3 == 6 && number4 == 6 && number5 == 6))
             {
                 totalnumber = number1 + number2 + number3 + number4 + number5;
-                textboxThreeOfAKindScore.Text = totalnumber.ToString();
+                textboxCarreScore.Text = totalnumber.ToString();
             }
 
-            //kleine straat
-            else if (number1 == 1 || number2 == 1 || number3 == 1 || number4 == 1 || number5 == 1 && number1 == 2 ||
-                number2 == 2 || number3 == 2 || number4 == 2 || number5 == 2 && number1 == 3 || number2 == 3 ||
-                number3 == 3 || number4 == 3 || number5 == 3 && number1 == 4 || number2 == 4 || number3 == 4 ||
-                number4 == 4 || number5 == 4 || number1 == 2 || number2 == 2 || number3 == 2 || number4 == 2 ||
-                number5 == 2 && number1 == 3 || number2 == 3 || number3 == 3 || number4 == 3 || number5 == 3 && number1 == 4 ||
-                number2 == 4 || number3 == 4 || number4 == 4 || number5 == 4 && number1 == 5 || number2 == 5 || number3 == 5 ||
-                number4 == 5 || number5 == 5 || number1 == 3 || number2 == 3 || number3 == 3 || number4 == 3 || number5 == 3 && (number1 == 4 ||
-                number2 == 4 || number3 == 4 || number4 == 4 || number5 == 4) && number1 == 5 || number2 == 5 || number3 == 5 || number4 == 5 || number5 == 5 &&
-                number1 == 6 || number2 == 6 || number3 == 6 || number4 == 6 || number5 == 6)
-                {
-                    totalnumber = number1 + number2 + number3 + number4 + number5;
-                    textboxKleineStraatScore.Text = totalnumber.ToString();
-                }
 
             //grote straat
-            else if (number1 == 1 || number2 == 1 || number3 == 1 || number4 == 1 || number5 == 1 &&
-                number1 == 2 || number2 == 2 || number3 == 2 || number4 == 2 || number5 == 2 &&
-                number1 == 3 || number2 == 3 || number3 == 3 || number4 == 3 || number5 == 3 &&
-                number1 == 4 || number2 == 4 || number3 == 4 || number4 == 4 || number5 == 4 &&
-                number1 == 5 || number2 == 5 || number3 == 5 || number4 == 5 || number5 == 5 || number1 == 2 || number2 == 2 || number3 == 2 || number4 == 2 || number5 == 2 &&
-                number1 == 3 || number2 == 3 || number3 == 3 || number4 == 3 || number5 == 3 && number1 == 4 || number2 == 4 || number3 == 4 || number4 == 4 || number5 == 4 &&
-                number1 == 5 || number2 == 5 || number3 == 5 || number4 == 5 || number5 == 5 && number1 == 6 || number2 == 6 || number3 == 6 || number4 == 6 || number5 == 6)
+
+            i[0] = number1;
+            i[1] = number2;
+            i[2] = number3;
+            i[3] = number4;
+            i[4] = number5;
+
+            Array.Sort(i);
+
+            if (((i[0] == 1) &&
+                 (i[1] == 2) &&
+                 (i[2] == 3) &&
+                 (i[3] == 4) &&
+                 (i[4] == 5)) ||
+                ((i[0] == 2) &&
+                 (i[1] == 3) &&
+                 (i[2] == 4) &&
+                 (i[3] == 5) &&
+                 (i[4] == 6)))
             {
-                totalnumber = number1 + number2 + number3 + number4 + number5;
-                textboxGroteStraatScore.Text = totalnumber.ToString();
+                textboxGroteStraatScore.Text = 40.ToString();
             }
 
 
 
+
+
+            //Kleine straat
+
+            i[0] = number1;
+            i[1] = number2;
+            i[2] = number3;
+            i[3] = number4;
+            i[4] = number5;
+
+            Array.Sort(i);
+
+            for (int j = 0; j < 4; j++)
+            {
+                int temp = 0;
+                if (i[j] == i[j + 1])
+                {
+                    temp = i[j];
+
+                    for (int k = j; k < 4; k++)
+                    {
+                        i[k] = i[k + 1];
+                    }
+
+                    i[4] = temp;
+                }
+            }
+
+            if (((i[0] == 1) && (i[1] == 2) && (i[2] == 3) && (i[3] == 4)) ||
+                ((i[0] == 2) && (i[1] == 3) && (i[2] == 4) && (i[3] == 5)) ||
+                ((i[0] == 3) && (i[1] == 4) && (i[2] == 5) && (i[3] == 6)) ||
+                ((i[1] == 1) && (i[2] == 2) && (i[3] == 3) && (i[4] == 4)) ||
+                ((i[1] == 2) && (i[2] == 3) && (i[3] == 4) && (i[4] == 5)) ||
+                ((i[1] == 3) && (i[2] == 4) && (i[3] == 5) && (i[4] == 6)))
+            {
+                textboxKleineStraatScore.Text = 30.ToString();
+            }
+
+            /*verandert nummer in dice, je kan section1 veranderen naar number1 enz. maar anders moet ik dat aanpassen wat weer veel tijd gaat kosten.
+              Dus doe ik dat nu niet*/
+            
             if (section1 == 1.ToString())
             {
              dice1.Text = "⚀";
@@ -327,19 +367,28 @@ namespace Yahtzee
                 dice5.Text = "⚅";
                 }
 
-      
+            
+            if (keuzeKleineStraat.Visibility == Visibility.Collapsed)
+            {
+                keuzeKleineStraat.Visibility = Visibility.Visible;
+            }
 
-           
+            if (keuzeGroteStraat.Visibility == Visibility.Collapsed)
+            {
+                keuzeGroteStraat.Visibility = Visibility.Visible;
+            }
+
+
         }
 
         private void Rules_click(object sender, RoutedEventArgs e)
         {
-            
+            GameRules page1 = new GameRules();
+            this.Content = page1;
         }
 
         private void Lock1_click(object sender, RoutedEventArgs e)
         { 
-        
             
         }
 
@@ -363,5 +412,57 @@ namespace Yahtzee
            
         }
 
+        private void KeuzeGroteStraat_click(object sender, RoutedEventArgs e)
+        {
+
+            if (textboxKleineStraatScore.Text.Length > 0.3)
+            {
+                try
+                {
+                    textboxKleineStraatScore.Text = textboxKleineStraatScore.Text.Remove(textboxKleineStraatScore.Text.Length - 2);
+                }
+                catch
+                {
+                    textboxKleineStraatScore.Text += "";
+                }
+
+                if (keuzeGroteStraat.Visibility == Visibility.Visible)
+                {
+                    keuzeGroteStraat.Visibility = Visibility.Collapsed;
+                }
+
+                if (keuzeKleineStraat.Visibility == Visibility.Visible)
+                {
+                    keuzeKleineStraat.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+        
+
+        private void KeuzeKleineStraat_click(object sender, RoutedEventArgs e)
+        {
+            if (textboxGroteStraatScore.Text.Length > 1)
+            {
+                try
+                {
+                    textboxGroteStraatScore.Text = textboxGroteStraatScore.Text.Remove(textboxGroteStraatScore.Text.Length - 2);
+                }
+                catch
+                {
+                    textboxGroteStraatScore.Text += "";   
+                }
+
+            if (keuzeKleineStraat.Visibility == Visibility.Visible)
+            {
+              keuzeKleineStraat.Visibility = Visibility.Collapsed;
+            }
+
+            if (keuzeGroteStraat.Visibility == Visibility.Visible)
+            {
+              keuzeGroteStraat.Visibility = Visibility.Collapsed;
+            }
+
+            }
+        }
     }
 }
